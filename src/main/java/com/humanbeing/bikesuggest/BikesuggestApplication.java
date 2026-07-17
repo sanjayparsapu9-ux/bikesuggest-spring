6c39d16ad26d4bff47373f30434b145ff6ccb258
sanjayparsapu9-ux <sanjayparsapu9@gmail.com>
2026-07-17 15:12:32 +0530

    first

---
+++ src/main/java/com/humanbeing/bikesuggest/BikesuggestApplication.java
@@ -0,0 +1,13 @@
+package com.humanbeing.bikesuggest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikesuggestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/humanbeing/bikesuggest/BikesuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humanbeing.bikesuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
